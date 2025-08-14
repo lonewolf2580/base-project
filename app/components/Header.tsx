4340c29a1d93939eff9d9183d2145d5cc8cf2be7
Linus Ngwee <linuslincom@gmail.com>
2025-08-14 12:58:06 +0100

    Connect button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,63 @@ import {
   WalletDropdown,
   WalletDropdownDisconnect,
 } from "@coinbase/onchainkit/wallet";
+"use client";
+
+import {
+  useMiniKit,
+  useAddFrame,
+  useOpenUrl,
+  useClose
+} from "@coinbase/onchainkit/minikit";
+import { useEffect, useMemo, useState, useCallback } from "react";
+import { Button } from "./DemoComponents";
+import { Icon } from "./DemoComponents";
 export default function Header() {
+  const { setFrameReady, isFrameReady, context } = useMiniKit();
+  const [frameAdded, setFrameAdded] = useState(false);
+  const [activeTab, setActiveTab] = useState("home");
+
+  const addFrame = useAddFrame();
+  const openUrl = useOpenUrl();
+  const close = useClose();
+
+  useEffect(() => {
+    if (!isFrameReady) {
+      setFrameReady();
+    }
+  }, [setFrameReady, isFrameReady]);
+
+  const handleAddFrame = useCallback(async () => {
+    const frameAdded = await addFrame();
+    setFrameAdded(Boolean(frameAdded));
+  }, [addFrame]);
+
+  const saveFrameButton = useMemo(() => {
+    if (context && !context.client.added) {
+      return (
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={handleAddFrame}
+          className="text-[var(--app-accent)] p-4"
+          icon={<Icon name="plus" size="sm" />}
+        >
+          Save Frame
+        </Button>
+      );
+    }
+
+    if (frameAdded) {
+      return (
+        <div className="flex items-center space-x-1 text-sm font-medium text-[#0052FF] animate-fade-out">
+          <Icon name="check" size="sm" className="text-[#0052FF]" />
+          <span>Saved</span>
+        </div>
+      );
+    }
+
+    return null;
+  }, [context, frameAdded, handleAddFrame]);
   return (
     <header className="flex justify-between items-center mb-3 h-11">
         <div>
@@ -32,7 +88,7 @@ export default function Header() {
                 </Wallet>
             </div>
         </div>
-              {/* <div>{saveFrameButton}</div> */}
+              <div>{saveFrameButton}</div>
     </header>
   )
 }
